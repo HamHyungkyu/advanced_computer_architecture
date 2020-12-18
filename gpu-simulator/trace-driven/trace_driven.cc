837ce7fc77d3488c292acd15e254250774893cd5
gusdnr9779 <gusdnr9779@postech.ac.kr>
2020-12-18 22:33:29 +0900

    hyunuk-link_and_page_manager

--- gpu-simulator/trace-driven/trace_driven.cc
+++ gpu-simulator/trace-driven/trace_driven.cc
@@ -867,10 +867,14 @@ void trace_config::set_latency(unsigned category, unsigned &latency,
 void trace_gpgpu_sim::createSIMTCluster()
 {
   m_cluster = new simt_core_cluster *[m_shader_config->n_simt_clusters];
-  for (unsigned i = 0; i < m_shader_config->n_simt_clusters; i++)
+  for (unsigned i = 0; i < m_shader_config->n_simt_clusters; i++) {
     m_cluster[i] =
         new trace_simt_core_cluster(this, i, m_shader_config, m_memory_config,
                                     m_shader_stats, m_memory_stats);
+
+    //hyunuk
+    m_cluster[i]->set_link(m_link);
+  }
 }
 
 void trace_simt_core_cluster::create_shader_core_ctx()
